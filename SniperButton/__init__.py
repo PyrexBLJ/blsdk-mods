c25fcb93050fb62aa21e02f0b08cc455f0263233
PyrexBLJ <lazr.modzz@gmail.com>
2022-05-25 23:13:59 -0400

    Update to be more reliable
    and switch to a slider + bool

--- SniperButton/__init__.py
+++ SniperButton/__init__.py
@@ -7,51 +7,65 @@ class Main(ModMenu.SDKMod):
     Name: str = "Sniper Button"
     Description: str = "<font size='20' color='#00ffe8'>Sniper Button</font>\n\n" \
     "Automatically changes aim sensitivity while aiming\n\n" \
-        "Default rate halves sensitivity\n\n" \
             "change rate can be modified in mod settings"
     Author: str = "PyrexBLJ"
-    Version: str = "1.0.0"
+    Version: str = "1.0.1"
     SaveEnabledState: ModMenu.EnabledSaveType = ModMenu.EnabledSaveType.LoadWithSettings
 
     Types: ModMenu.ModTypes = ModMenu.ModTypes.Utility
-    SupportedGames: ModMenu.Game = ModMenu.Game.BL2
+    SupportedGames: ModMenu.Game = ModMenu.Game.BL2 | ModMenu.Game.TPS
 
-    aimRate = "0.5"
-    aimRateNum = 0.5
     defaultMouseSense = 60
+    aimRate = 1
+    increase = False
 
     def __init__(self) -> None:
         super().__init__()
-        self.MySpinner = ModMenu.Options.Spinner(
+        self.MySlider = ModMenu.Options.Slider(
             Caption="Aimed Rate",
-            Description="How much sensitivity should be multiplied by while aiming",
-            StartingValue="0.5",
-            Choices=[ "0.1", "0.25", "0.5", "0.75", "1.0", "1.1", "1.25", "1.5", "1.75", "2.0", "2.25", "2.5", "2.75", "3.0"]
+            Description="How much sensitivity should be modified by while aiming",
+            StartingValue = 1,
+            MinValue = 1,
+            MaxValue = 5,
+            Increment = 1,
+        )
+        self.MyBoolean = ModMenu.Options.Boolean(
+            Caption="Increase Speed",
+            Description="True = faster aiming   False = slower aiming",
+            StartingValue=True,
+            Choices=["No", "Yes"]  # False, True
         )
         
         self.Options = [
-            self.MySpinner,
+            self.MySlider,
+            self.MyBoolean
         ]
 
     def ModOptionChanged(self, option: ModMenu.Options.Base, new_value) -> None:
-        if option == self.MySpinner:
+        if option == self.MySlider:
             self.aimRate = new_value
-            self.aimRateNum = atof(new_value)
+        if option == self.MyBoolean:
+            self.increase = new_value
 
     @ModMenu.Hook("WillowGame.WillowWeapon.StartZoom")
-    def onStartZoom(self, caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
+    def onStartZoom(self, caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> None:
         controller = unrealsdk.GetEngine().GamePlayers[0].Actor
-        self.defaultMouseSense = controller.PlayerInput.MouseSensitivity
-        controller.PlayerInput.MouseSensitivity = self.defaultMouseSense * self.aimRateNum
+        if self.increase == True:
+            controller.PlayerInput.MouseSensitivity = controller.PlayerInput.MouseSensitivity * self.aimRate
+        elif self.increase == False: 
+            controller.PlayerInput.MouseSensitivity = controller.PlayerInput.MouseSensitivity / self.aimRate
+
         return True
 
     @ModMenu.Hook("WillowGame.WillowWeapon.StopZoom")
-    def onStopZoom(self, caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
+    def onStopZoom(self, caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> None:
         controller = unrealsdk.GetEngine().GamePlayers[0].Actor
         controller.PlayerInput.MouseSensitivity = self.defaultMouseSense
         return True
 
     def Enable(self) -> None:
+        controller = unrealsdk.GetEngine().GamePlayers[0].Actor
+        self.defaultMouseSense = controller.PlayerInput.MouseSensitivity
         super().Enable()
 
     def Disable(self) -> None:
